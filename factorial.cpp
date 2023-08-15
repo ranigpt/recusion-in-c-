a334b52dac09e66abf64dcad596592e4db5e342f
Rani gupta <118204551+ranigpt@users.noreply.github.com>
2023-08-15 22:50:40 +0530

    factorial.cpp

--- factorial.cpp
+++ factorial.cpp
@@ -20,3 +20,90 @@ int main()
 
     return 0;
 }
+
+//reverse of the array using recursion
+
+
+#include <bits/stdc++.h>
+
+using namespace std;
+
+
+
+
+void f(int i,int arr[],int n)
+{
+    if(i>=n/2) return;
+    
+    swap(arr[i],arr[n-i-1]);
+    f(i+1,arr,n);
+}
+
+int main()
+{
+    int n;
+    cin>>n;
+    int arr[n];
+    for(int i=0;i<n;i++) cin>>arr[i];
+    
+    f(0,arr,n);
+    for(int i=0;i<n;i++) cout<<arr[i]<<" ";
+
+    return 0;
+}
+
+
+
+// check palindrom or not
+
+
+#include <bits/stdc++.h>
+
+using namespace std;
+
+bool f(int i, string &s)
+{
+  if(i >= s.size()/2) return true;
+
+  if(s[i]!=s[s.size()-i-1]) return false;
+
+  return f(i+1,s);
+  
+}
+
+
+int main() {
+  
+  string var;
+  cin>>var;
+cout<<f(0,var);
+
+  return 0;
+  //cin>>var;
+}
+
+
+//fibbonacci series in recursion or example of multiple recursion calls
+
+
+#include <bits/stdc++.h>
+using namespace std;
+
+int f(int n){
+  if(n<=1) return n;
+
+  int last=f(n-1);
+  int slast=f(n-2);
+
+  return last+slast;
+}
+
+int main() {
+ 
+
+  int num;
+  cin>>num;
+  int finbo;
+  finbo=f(num);
+  cout<<finbo;
+}
